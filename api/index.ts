5955afb34b8d02cc6d217a1335555b512956976b
CamiloTECH <camilomontoya10@live.com>
2022-06-10 12:27:33 -0500

    Deployd back

--- api/index.ts
+++ api/index.ts
@@ -4,6 +4,6 @@ import { sequelize } from "./src/db"
 sequelize.sync({force:false, logging:false})
 .then(()=>{
   console.log("Base de datos conectada")
-  app.listen(process.env.PORT,()=>console.log("Escuchando en "+process.env.PORT))
+  app.listen(process.env.PORT,()=>console.log("Escuchando en "+ process.env.PORT))
 })
 .catch((err)=>console.log(err))
