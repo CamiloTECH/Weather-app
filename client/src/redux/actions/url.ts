cbd24f4bf4d8bf164b2f199828d662e6220d4eca
Camilo Montoya <camilo.montoya@masterbase.com>
2023-08-13 14:32:13 -0500

    update environment variables

--- client/src/redux/actions/url.ts
+++ client/src/redux/actions/url.ts
@@ -1,4 +1,5 @@
-const URL: string = "http://localhost:3001";
-// const URL: string = import.meta.env.VITE_BACKEND_URL ||"http://localhost:3001";
+// const URL: string = "http://localhost:3001";
+
+const URL: string = import.meta.env.VITE_BACKEND_URL;
 
 export default URL;
